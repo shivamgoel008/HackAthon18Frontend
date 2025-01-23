693ee0abd9050b113d230b11ef8cb5d8bae2de41
Shivam Kumar <55030452+shivamgoel008@users.noreply.github.com>
2025-01-23 22:03:35 +0530

    node modules

--- backend/service/chat_service.py
+++
@@ -1,9 +0,0 @@
-from .agentic_workflow import TriageAssistant
-
-
-class ChatService:
-    def __init__(self):
-        self.assistant = TriageAssistant()
-
-    def query(self, query: str):
-        return self.assistant.query(query)
